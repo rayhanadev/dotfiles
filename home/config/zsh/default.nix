188d95c2e491e530d29d60fa7b2e75018cf66146
ray <me@rayhanadev.com>
2024-06-06 15:22:29 -0500

    fix: broken homebrew config

--- home/config/zsh/default.nix
+++ home/config/zsh/default.nix
@@ -6,6 +6,10 @@
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
       export GPG_TTY=$(tty)
       export SSH_AUTH_SOCK=\$\{HOME\}/.gnupg/S.gpg-agent.ssh
+      export HOMEBREW_NO_ANALYTICS=1
+    '';
+    profileExtra = ''
+      eval "$(/opt/homebrew/bin/brew shellenv)"
     '';
   };
 
