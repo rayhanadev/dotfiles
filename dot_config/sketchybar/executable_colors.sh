6defa465b00e5690bf919f783ce652f8908a7eac
Rayhan Noufal Arayilakath <me@rayhanadev.com>
2026-05-31 14:40:44 -0700

    feat: sketchybar burnt amber theme from wallpaper

--- dot_config/sketchybar/executable_colors.sh
+++ dot_config/sketchybar/executable_colors.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-export BLACK=0xff101010
-export WHITE=0xffb9b9b9
-export RED=0xff7c7c7c
-export GREEN=0xff8e8e8e
-export BLUE=0xff686868
-export YELLOW=0xffa0a0a0
-export ORANGE=0xff747474
-export MAGENTA=0xffe3e3e3
-export GREY=0xff525252
+# Burnt amber — pulled from claudio-guglieri blueprint wallpaper
+export BLACK=0xff1f1104        # deep shadow (top of wallpaper)
+export WHITE=0xffe0a55a        # light amber (text/icons)
+export RED=0xffc06c03          # bright grid amber (active highlight)
+export GREEN=0xff9d4c02        # mid amber
+export BLUE=0xffb35f04         # amber (apple/slider/cpu accent)
+export YELLOW=0xffc77e1a       # warm amber
+export ORANGE=0xff9d4c02       # burnt orange
+export MAGENTA=0xffe8b070      # lightest glow
+export GREY=0xff6e3608         # dim brown-orange (inactive)
 export TRANSPARENT=0x00000000
-export BG0=0x1A101010
-export BG1=0x60101010
-export BG2=0x60525252
+export BG0=0x1A1f1104
+export BG1=0x601f1104
+export BG2=0x605b2c03
 
 # General bar colors
 export BAR_COLOR=$BG0
